6b5dc11586eabe295e294a77390324dcd8111ac5
Luiz Amorim <luizfelipeamorim9@.com>
2025-05-09 23:12:08 -0300

    FEEDBAKS

--- push.bat
+++ push.bat
@@ -1,5 +1,5 @@
 @echo off
-cd  D:\Users\luif\jogos,arquivos(D:)\Projetofitmaxpython\fitmaxonpython
+cd  D:\Usuarios\luif\jogos,arquivos(D:)\Projetofitmaxpython\fitmaxonpython
 
 git add .
 git commit -m "FEEDBAKS"
