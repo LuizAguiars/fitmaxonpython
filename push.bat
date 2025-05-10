1bf78dfb47f3a94e46ae8bccb31f7139609f9019
Luiz Amorim <luizfelipeamorim9@.com>
2025-05-10 12:09:32 -0300

    Ajuste final tela usu├írio enviando feedback front-end e rota criado no back-end apenas para linkar 10/05/2025 12:09:32,92

--- push.bat
+++ push.bat
@@ -1,6 +1,35 @@
-cd /d "D:\Projetofitmaxpython\fitmaxonpython"
-git init
+
+@echo off
+REM Altere para o diretório do seu projeto
+cd /d D:\Projetofitmaxpython\fitmaxonpython
+
+REM Troca para a branch develop
+git checkout develop
+
+REM Adiciona todas as alterações
 git add .
-git commit -m "ajuste final da tela feedback front-end"
-git checkout -b develop
-git push --set-upstream origin develop
+
+REM Comita com mensagem automática com data e hora
+set DATAHORA=%date% %time%
+
+git commit -m "Ajuste final tela usuário enviando feedback front-end e rota criado no back-end apenas para linkar %DATAHORA%"
+
+
+
+REM Atualiza repositório local
+git fetch origin
+
+REM Verifica se main está à frente da develop
+git merge-base --is-ancestor origin/develop origin/main
+IF %ERRORLEVEL% EQU 1 (
+    echo [INFO] A branch develop está atrás da main. Fazendo merge...
+    git merge origin/main -m "Merge automático da main para develop"
+) ELSE (
+    echo [INFO] A branch develop já está atualizada com a main.
+)
+
+REM Envia para a branch develop no remoto
+git push origin develop 
+
+echo [SUCESSO] Push realizado na branch DEVELOP com sucesso!
+pause
