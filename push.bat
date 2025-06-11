9353ce3c8b64395ea95122efbd704cf4c8cb11e6
MateusHenriSouza <mateushenricruz@gmail.com>
2025-06-11 18:21:06 -0300

    Anota├º├úo dos erros de valida├º├úo e alguns ajustes feitos

--- push.bat
+++ push.bat
@@ -11,7 +11,7 @@ git add .
 REM Comita com mensagem automatica com data e hora
 set DATAHORA=%date% %time%
 
-git commit -m "Anotação dos erros de validação
+git commit -m "Anotação dos erros de validação e alguns ajustes feitos
  %DATAHORA%"
 
 
