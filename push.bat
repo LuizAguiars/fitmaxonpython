94f6d20e106e4e38d736e84ff80614c5803d39ed
MateusHenriSouza <mateushenricruz@gmail.com>
2025-06-12 18:59:34 -0300

    Barras de pesquisas removidas

--- push.bat
+++ push.bat
@@ -11,7 +11,7 @@ git add .
 REM Comita com mensagem automatica com data e hora
 set DATAHORA=%date% %time%
 
-git commit -m "adicao de horario personal final
+git commit -m "Barras de pesquisas removidas
  %DATAHORA%"
 
 
