84534f4e91acd436ccc61affffdaf2574a186f99
Luiz felipe aguiar de souza <161994067+LuizAguiars@users.noreply.github.com>
2025-06-22 18:18:08 -0300

    rework tela inicial  22/06/2025 18:18:07,90

--- push.bat
+++ push.bat
@@ -11,7 +11,7 @@ git add .
 REM Comita com mensagem automatica com data e hora
 set DATAHORA=%date% %time%
 
-git commit -m "Ajustes no agendar aulas e graficos de relatorios  %DATAHORA%"
+git commit -m "rework tela inicial  %DATAHORA%"
  %DATAHORA%"
 
 
