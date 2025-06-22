d1b2be1f5c65de4789bfb87552a7358c989d9a08
Luiz felipe aguiar de souza <161994067+LuizAguiars@users.noreply.github.com>
2025-06-22 20:07:14 -0300

    pequenas correcoes visuais  22/06/2025 20:07:14,63

--- push.bat
+++ push.bat
@@ -11,7 +11,7 @@ git add .
 REM Comita com mensagem automatica com data e hora
 set DATAHORA=%date% %time%
 
-git commit -m "rework tela inicial 2 (pequenos ajustes)  %DATAHORA%"
+git commit -m "pequenas correcoes visuais  %DATAHORA%"
  %DATAHORA%"
 
 
